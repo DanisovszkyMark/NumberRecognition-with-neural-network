3f38375170dc80c278610354f57198be7c75129d
DanisovszkyMark <32265752+DanisovszkyMark@users.noreply.github.com>
2019-06-25 13:00:40 +0200

    PaintProcessor osztály megírása
    Elvégzett feladatok: Egy olyan osztály megírása, mely képes a rajzolt "képet" kezelni. A rajzolt képet a neurális hálózatok számára át tudja alakítani elfogadható bemenetként illetve file-ba le tudjuk menteni esetleges későbbi felhasználás céljából.

---
+++ Software/NumberRecognition/NumberRecognition/Paint/PaintProcessor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace NumberRecognition.Paint
+{
+    class PaintProcessor
+    {
+        public static float[] PaintToInput(Grid grid)
+        {
+
+            float[] atmInput = new float[grid.Children.Count];
+
+            for (int i = 0; i < grid.Children.Count; i++)
+            {
+                if ((grid.Children[i] as ListBox).Background == Brushes.White) atmInput[i] = 1;
+            }
+
+            return atmInput;
+        }
+
+        public static bool SavePaint(Grid grid, int number, string fileName)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(fileName, true);
+
+                float[] atm = PaintToInput(grid);
+                for (int i = 0; i < atm.Length; i++)
+                {
+                    sw.Write(atm[i]);
+                }
+
+                string atmNumber = "";
+                for (int i = 0; i < 10; i++)
+                {
+                    if (i == number) atmNumber += "1";
+                    else atmNumber += "0";
+                }
+
+
+                sw.WriteLine("-" + atmNumber);
+                sw.Close();
+            }
+            catch { return false; }
+
+            return true;
+        }
+    }
+}
